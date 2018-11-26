357f4807e68c2144126375a65291c994e724b157
linyl <service@im050.com>
2018-11-26 18:19:58 +0800

    feature: get host file by system

--- main.go
+++ main.go
@@ -1,21 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"runtime"
 )
 
 var (
 	hosts []Host
-	)
+)
 
 func main() {
-	file, err := os.Open("C:\\Windows\\System32\\drivers\\etc\\hosts")
-	hosts = GetCurrentHosts(file)
+
+	file, err := os.Open(getHostsFile())
+	go func() {
+		fmt.Println("加载当前hosts")
+		hosts = GetCurrentHosts(file)
+	}()
 	if err != nil {
+		fmt.Println(err)
 		panic(err)
 	}
 	defer file.Close()
 	ServerStart()
+}
+
+func getHostsFile() string {
+	switch runtime.GOOS {
+	case "darwin":
+		return "/etc/hosts"
+	case "windows":
+		return "C:\\Windows\\System32\\drivers\\etc\\hosts"
+	case "linux":
+		return "/etc/hosts"
+	default:
+		return ""
+	}
 }
 
 func checkError(err error) {
