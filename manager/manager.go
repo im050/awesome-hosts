8fb5220a9703d77367045d9d83cb60545bf65365
linyl <service@im050.com>
2018-12-01 00:27:46 +0800

    feature: 调整host文件存储结构

--- manager/manager.go
+++ manager/manager.go
@@ -59,20 +59,20 @@ func New(hostsDir string) *Manager {
 }
 
 func (h *Manager) Init() *Manager {
-	exists, err := PathExists(h.hostsDir)
-
+	//third, init host groups
 	defer h.initGroups()
+	//second, init or load config into h.Config
+	defer h.loadConfig()
+	//first, backup system hosts as a new group
 	defer h.initSystemHosts()
-
+	exists, err := PathExists(h.hostsDir)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(0)
 	}
-
 	if exists {
 		return h
 	}
-
 	//create hosts dir
 	err = os.Mkdir(h.hostsDir, 0777)
 	if err != nil {
@@ -96,7 +96,6 @@ func (h *Manager) initSystemHosts() {
 		return
 	}
 	h.WriteHosts(GetHostFileName(h.DefaultGroupName), h.SystemHosts)
-	h.initConfigFile()
 }
 
 func (h *Manager) initGroups() {
@@ -108,16 +107,24 @@ func (h *Manager) GetHostDir() string {
 	return h.hostsDir
 }
 
-func (h *Manager) initConfigFile() {
-	h.Config = Config{
-		Groups:               []HostGroupData{{Name: h.DefaultGroupName, Enabled: true, LastUpdatedTimestamp: 0}},
-		LastUpdatedTimestamp: 0,
-		LastSyncTimestamp:    0,
+func (h *Manager) loadConfig() {
+	exists, _ := PathExists(h.hostsDir + "/" + h.ConfigFileName)
+	if exists {
+		h.loadConfigFromFile()
+	} else {
+		h.Config = Config{
+			Groups:               []HostGroupData{{Name: h.DefaultGroupName, Enabled: true, LastUpdatedTimestamp: 0}},
+			LastUpdatedTimestamp: 0,
+			LastSyncTimestamp:    0,
+		}
+		h.persistConfig()
 	}
-	jsonText, err := json.Marshal(h.Config)
+}
+
+func (h *Manager) loadConfigFromFile() {
+	fileByte, err := ioutil.ReadFile(h.hostsDir + "/" + h.ConfigFileName)
 	ErrorAndExitWithLog(err)
-	err = ioutil.WriteFile(h.hostsDir+"/"+h.ConfigFileName, jsonText, 0644)
-	ErrorAndExitWithLog(err)
+	ErrorAndExitWithLog(json.Unmarshal(fileByte, &h.Config))
 }
 
 func (h *Manager) GetHosts(file *os.File) Hosts {
@@ -125,10 +132,6 @@ func (h *Manager) GetHosts(file *os.File) Hosts {
 	//each file line by line
 	lineIndex := -1
 	var hosts []Host
-	groupEnableMap := map[string]bool{}
-	for _, c := range h.Config.Groups {
-		groupEnableMap[c.Name] = c.Enabled
-	}
 	for {
 		line, _, err := br.ReadLine()
 		lineString := strings.TrimSpace(string(line))
@@ -194,7 +197,11 @@ func (h *Manager) WriteHosts(name string, hosts Hosts) {
 
 func (h *Manager) GetGroups() []Group {
 	var groups []Group
-	fmt.Println(h.hostsDir)
+	groupEnableMap := map[string]bool{}
+	for _, c := range h.Config.Groups {
+		groupEnableMap[c.Name] = c.Enabled
+	}
+	fmt.Println(groupEnableMap)
 	files, _ := ioutil.ReadDir(h.hostsDir)
 	for _, f := range files {
 		groupInfo := strings.Split(f.Name(), ".")
@@ -206,13 +213,39 @@ func (h *Manager) GetGroups() []Group {
 			groupName = strings.Join(groupInfo[0:], ".")
 		}
 		transferGroupName(&groupName, true)
+		var enabled bool
+		value, exists := groupEnableMap[groupName]
+		if !exists {
+			enabled = false
+			//if group doesn't exists in config file, then add it.
+			h.addGroupToConfig(groupName, enabled, 0)
+		} else {
+			enabled = value
+		}
 		//read host file
 		file, err := os.Open(h.hostsDir + "/" + f.Name())
 		hosts := h.GetHosts(file)
 		ErrorAndExitWithLog(file.Close())
 		ErrorAndExitWithLog(err)
 		//append to groups
-		groups = append(groups, Group{Name: groupName, Enabled: false, Active: false, Hosts: hosts})
+		groups = append(groups, Group{Name: groupName, Enabled: enabled, Active: false, Hosts: hosts})
 	}
+	//refresh config file content
+	h.persistConfig()
 	return groups
+}
+
+func (h *Manager) persistConfig() {
+	jsonText, err := json.Marshal(h.Config)
+	ErrorAndExitWithLog(err)
+	err = ioutil.WriteFile(h.hostsDir+"/"+h.ConfigFileName, jsonText, 0666)
+	ErrorAndExitWithLog(err)
+}
+
+func (h *Manager) addGroupToConfig(groupName string, enabled bool, lastUpdatedTimestamp uint32) {
+	h.Config.Groups = append(h.Config.Groups, HostGroupData{
+		Name:                 groupName,
+		Enabled:              enabled,
+		LastUpdatedTimestamp: lastUpdatedTimestamp,
+	})
 }
