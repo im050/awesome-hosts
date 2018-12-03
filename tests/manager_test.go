fd5de24d60b4c16805ccd097f83a4f028f371839
linyl <service@im050.com>
2018-12-04 06:11:35 +0800

    feature: 增加单元测试、性能测试

---
+++ tests/manager_test.go
@@ -0,0 +1,52 @@
+package tests
+
+import (
+	"awesome-hosts/manager"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPattern(t *testing.T) {
+	m := manager.New("")
+	assert.Error(t, m.CheckIP("123"))
+	assert.NoError(t, m.CheckIP("127.0.0.1"))
+	assert.NoError(t, m.CheckIP("fe80::181c:65cd:d4ff:9885"))
+	assert.NoError(t, m.CheckIP("::1"))
+	assert.NoError(t, m.CheckDomain("localhost"))
+	assert.NoError(t, m.CheckDomain("www.baidu.com"))
+	assert.Error(t, m.CheckDomain("good,|>asdf.asdf"))
+	assert.Error(t, m.CheckDomain(".host"))
+	assert.Error(t, m.CheckDomain("host."))
+	assert.Error(t, m.CheckGroupName(",hel?lo"))
+	assert.Error(t, m.CheckGroupName("h<e>ll|\\o"))
+	assert.NoError(t, m.CheckGroupName("he.llo"))
+	assert.NoError(t, m.CheckGroupName("he-_llo"))
+	assert.NoError(t, m.CheckGroupName("Good boy"))
+}
+
+func BenchmarkCheckIP(b *testing.B) {
+	m := manager.New("")
+	for i := 0; i < b.N; i++ {
+		m.CheckIP("::1")
+	}
+}
+
+func BenchmarkCheckDomain(b *testing.B) {
+	m := manager.New("")
+	for i := 0; i < b.N; i++ {
+		m.CheckDomain("::1")
+	}
+}
+
+func BenchmarkCheckGroupName(b *testing.B) {
+	m := manager.New("")
+	for i := 0; i < b.N; i++ {
+		m.CheckGroupName("aaaaaaaaaaaaaa")
+	}
+}
+
+//func BenchmarkSync(b *testing.B) {
+//	m := manager.New(manager.GetUserHome() + "/.awesohosts")
+//	m.SudoPassword = "your root password here"
+//	m.SyncSystemHostsUnix()
+//}
